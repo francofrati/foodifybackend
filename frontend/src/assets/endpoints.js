4c0f50ebd28228290600439d2ff458b883e4f546
franco fraticelli <francofraticelliff@gmail.com>
2022-10-19 20:58:43 -0300

    first endpoint

--- frontend/src/assets/endpoints.js
+++ frontend/src/assets/endpoints.js
@@ -3,7 +3,7 @@ export const verifyRestaurantByIdURL = (id) => `http://localhost:3001/restaurant
 export const loginURL = 'http://localhost:3001/user/login'
 export const credsURL = 'http://localhost:3001/authentication'
 export const signUpURL = 'http://localhost:3001/user/signup'
-export const allRestaurantsURL = 'http://localhost:3001/restaurant'
+export const allRestaurantsURL = 'https://server-om6g.onrender.com/restaurant'
 export const oneRestaurantURL = (id) => `http://localhost:3001/restaurant/${id}`
 export const loginRestaurantURL = 'http://localhost:3001/restaurant/login'
 export const registerRestaurantURL = 'http://localhost:3001/restaurant/register'

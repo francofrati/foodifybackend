8ae835c2efb4c220fad0b8e8e3ea30d8cdcd0acf
franco fraticelli <francofraticelliff@gmail.com>
2022-10-19 21:27:32 -0300

    endpoints

--- frontend/src/assets/endpoints.js
+++ frontend/src/assets/endpoints.js
@@ -1,24 +1,24 @@
-export const userByIdURL = (id) => `http://localhost:3001/user/verify/${id}`
-export const verifyRestaurantByIdURL = (id) => `http://localhost:3001/restaurant/verify/${id}`
-export const loginURL = 'http://localhost:3001/user/login'
-export const credsURL = 'http://localhost:3001/authentication'
-export const signUpURL = 'http://localhost:3001/user/signup'
+export const userByIdURL = (id) => `https://server-om6g.onrender.com/user/verify/${id}`
+export const verifyRestaurantByIdURL = (id) => `https://server-om6g.onrender.com/restaurant/verify/${id}`
+export const loginURL = 'https://server-om6g.onrender.com/user/login'
+export const credsURL = 'https://server-om6g.onrender.com/authentication'
+export const signUpURL = 'https://server-om6g.onrender.com/user/signup'
 export const allRestaurantsURL = 'https://server-om6g.onrender.com/restaurant'
-export const oneRestaurantURL = (id) => `http://localhost:3001/restaurant/${id}`
-export const loginRestaurantURL = 'http://localhost:3001/restaurant/login'
-export const registerRestaurantURL = 'http://localhost:3001/restaurant/register'
-export const googleAuthURL = 'http://localhost:3001/user/google'
-export const accountVerificationURL = 'http://localhost:3001/user/verify'
-export const restaurantVerificationURL = 'http://localhost:3001/restaurant/verify'
-export const preVerifyRestaurantRegistrationURL = 'http://localhost:3001/restaurant/register/verify'
-export const getOrdersByRestId = (id) => `http://localhost:3001/orders/ordenes?restId=${id}`
-export const shopPanelURL = (option) => `http://localhost:3001/shop?option=${option}`
-export const shopPasswordChangeURL = `http://localhost:3001/shop/password`
-export const getAllFoodURL = `http://localhost:3001/foods`
-export const getOrdersByUserIdURL = (userId) =>`http://localhost:3001/orders/${userId}`
-export const addFavoriteRestaurantURL = `http://localhost:3001/user/favorite`
-export const getFavoritesRestaurantsURL = (userId)=>`http://localhost:3001/user/favorite/${userId}`
-export const getFoodByRestaurantURL = (restId)=>`http://localhost:3001/foods/foodsRestaurant/${restId}`
-export const foodUpdateRestaurant = `http://localhost:3001/shop/food`
-export const getShopInfoURL = (restId) =>`http://localhost:3001/shop/${restId}`
-export const reviewURL = `http://localhost:3001/reviews`
+export const oneRestaurantURL = (id) => `https://server-om6g.onrender.com/restaurant/${id}`
+export const loginRestaurantURL = 'https://server-om6g.onrender.com/restaurant/login'
+export const registerRestaurantURL = 'https://server-om6g.onrender.com/restaurant/register'
+export const googleAuthURL = 'https://server-om6g.onrender.com/user/google'
+export const accountVerificationURL = 'https://server-om6g.onrender.com/user/verify'
+export const restaurantVerificationURL = 'https://server-om6g.onrender.com/restaurant/verify'
+export const preVerifyRestaurantRegistrationURL = 'https://server-om6g.onrender.com/restaurant/register/verify'
+export const getOrdersByRestId = (id) => `https://server-om6g.onrender.com/orders/ordenes?restId=${id}`
+export const shopPanelURL = (option) => `https://server-om6g.onrender.com/shop?option=${option}`
+export const shopPasswordChangeURL = `https://server-om6g.onrender.com/shop/password`
+export const getAllFoodURL = `https://server-om6g.onrender.com/foods`
+export const getOrdersByUserIdURL = (userId) =>`https://server-om6g.onrender.com/orders/${userId}`
+export const addFavoriteRestaurantURL = `https://server-om6g.onrender.com/user/favorite`
+export const getFavoritesRestaurantsURL = (userId)=>`https://server-om6g.onrender.com/user/favorite/${userId}`
+export const getFoodByRestaurantURL = (restId)=>`https://server-om6g.onrender.com/foods/foodsRestaurant/${restId}`
+export const foodUpdateRestaurant = `https://server-om6g.onrender.com/shop/food`
+export const getShopInfoURL = (restId) =>`https://server-om6g.onrender.com/shop/${restId}`
+export const reviewURL = `https://server-om6g.onrender.com/reviews`
